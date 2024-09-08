d1fa5cc132ec0cf474bea30df3aabe0caad80ecf
Kushagra  Srivastav <76401345+Kushagra7777@users.noreply.github.com>
2024-09-08 10:34:44 +0530

    Update ARC_functions.py

--- ARC_functions.py
+++ ARC_functions.py
@@ -5,7 +5,24 @@ import random
 
 # assumes an available local installation of ao_core; refer to https://github.com/aolabsai/ao_core?tab=readme-ov-file#installing-ao_core
 import ao_core as ao
-from arch_ARC import arcArch
+import ao_arch as ar
+
+neurons_x = 30  # Number of neurons in the x direction (global variable)
+neurons_y = 30  # Number of neurons in the y direction
+description = "ARC-AGI agent"  # Description of the agent
+
+# Initialize the input and output architecture with 4 neurons per channel
+arch_i = [4 for x in range(neurons_x * neurons_y)]  # Input architecture (4 neuron per channel for encoding colors in binary) 
+arch_z = [4 for x in range(neurons_x * neurons_y)]  # Output architecture 
+arch_c = []  
+connector_function = "nearest_neighbour_conn"  # Function for connecting neurons
+
+
+Z2I_connections = True #wether want Z to I connection or not. If not specified, by default it's False. 
+connector_parameters = [4, 4, neurons_x, neurons_y, Z2I_connections]  #ax, dg, neurons_x, neurons_y and Z2I connection (True or default False)
+
+# Create the architecture using the Arch class from the ao_arch library
+arcArch = ar.Arch(arch_i, arch_z, arch_c, connector_function, connector_parameters, description)
 
 
 arcAgent = ao.Agent( arcArch )
